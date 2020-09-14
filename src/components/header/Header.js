5e858e24209e9ee5aa60cb190545cb31404ae923
shams.youssef <shams.youssef@3ddx.com>
2020-09-14 02:19:04 +0200

    create new react project and add header

---
+++ src/components/header/Header.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import Search from '../search/search';
+
+import logo from '../../assets/logoArbic.svg';
+
+const Header = () => {
+	return (
+		<Navbar expand='lg' sticky='top' className='navbar-container'>
+			<Navbar.Brand className='logo' href=''>
+				<img
+					src={logo}
+					width='140'
+					className='d-inline-block align-top'
+					alt='Logo'
+				/>
+			</Navbar.Brand>
+			<Navbar.Toggle aria-controls='basic-navbar-nav' className='p-0' />
+			<Navbar.Collapse id='basic-navbar-nav'>
+				<Nav className='mr-auto'>
+					<Nav.Link href='#home' className='translate'>
+						ENG
+					</Nav.Link>
+					<Nav.Link href='#home'>تسجيل الدخول</Nav.Link>
+					<NavDropdown title='تتبع شحنتك' id='basic-nav-dropdown'>
+						<Search></Search>
+					</NavDropdown>
+				</Nav>
+				<Nav className='mr-auto'>
+					<Nav.Link href='#home'>كلم المبيعات</Nav.Link>
+					<Nav.Link href='#home'>الأسعار</Nav.Link>
+					<Nav.Link href='#home'>الرئيسية</Nav.Link>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	);
+};
+
+export default Header;
---
+++ src/components/search/search.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Search = () => {
+	return (
+		<div className='search-container'>
+			<div>تتبع شحنتك</div>
+			<div>ادخل رقم الشحنة و تابع شحنتك أول بأول</div>
+		</div>
+	);
+};
+
+export default Search;
