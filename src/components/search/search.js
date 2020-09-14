9a7064698ab2dc3d5797a25a5d6101fd20db21ca
shams.youssef <shams.youssef@3ddx.com>
2020-09-14 02:57:16 +0200

    add search section

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -3,8 +3,21 @@ import React from 'react';
 const Search = () => {
 	return (
 		<div className='search-container'>
-			<div>تتبع شحنتك</div>
-			<div>ادخل رقم الشحنة و تابع شحنتك أول بأول</div>
+			<div className='search-header'>تتبع شحنتك</div>
+			<div className='search-label'>ادخل رقم الشحنة و تابع شحنتك أول بأول</div>
+
+			<div className='form-container'>
+				<button id='submit-button' type='submit'>
+					<i class='fa fa-search'></i>
+				</button>
+				<input
+					type='text'
+					class='pop_text'
+					placeholder='رقم الشحنة'
+					id='track-number'
+					name='track_number'
+				/>
+			</div>
 		</div>
 	);
 };
